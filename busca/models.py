a208b0a15e240d388635f148a26991508277d9c9
veemon18 <lucasdrain18@hotmail.com>
2025-06-28 22:34:06 -0300

    Update models.py

--- busca/models.py
+++ busca/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Farmacia(models.Model):
+    nome = models.CharField(max_length=255)
+    endereco = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=20, blank=True)
+    horario_funcionamento = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Medicamento(models.Model):
+    nome = models.CharField(max_length=255)
+    nome_generico = models.CharField(max_length=255)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    farmacia = models.ForeignKey(Farmacia, related_name='medicamentos', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
