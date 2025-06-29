4d0c27db1bbe749b53ad8b6805ef1675ec668b56
veemon18 <lucasdrain18@hotmail.com>
2025-06-28 22:41:06 -0300

    Update urls.py

--- cliente/urls.py
+++ cliente/urls.py
@@ -2,9 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ver_cliente/', views.ver_cliente, name="ver_cliente"),
-    path('criar_cliente/', views.criar_cliente, name="criar_cliente"),
-    path('login/', views.login, name="login")
-  
-
+    path('busca/', views.busca, name='busca'),
 ]
