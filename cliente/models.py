b276557ba366bda2633f4904650f987911e718e2
veemon18 <lucasdrain18@hotmail.com>
2025-06-28 21:40:35 -0300

    Update models.py

--- cliente/models.py
+++ cliente/models.py
@@ -3,8 +3,6 @@ from django.db import models
 
 class Cliente(AbstractUser):
     telefone = models.CharField(max_length=20, blank=True)
-    email = models.EmailField(unique=True)
-    nome = models.CharField(max_length=255)
     endereco = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
