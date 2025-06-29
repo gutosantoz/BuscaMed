25dfd3056fab7b25a6d45b78a089a8b6817cbc44
veemon18 <lucasdrain18@hotmail.com>
2025-06-28 22:35:55 -0300

    Update views.py

--- busca/views.py
+++ busca/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Medicamento, Farmacia
 
 def busca(request):
-    return render(request, 'busca.html')
+    query = request.GET.get('q')  # Recebe o termo de pesquisa via GET
+    if query:
+        # Realiza a busca no banco de dados
+        medicamentos = Medicamento.objects.filter(nome__icontains=query)
+    else:
+        medicamentos = Medicamento.objects.none()
+
+    resultados = []
+    for medicamento in medicamentos:
+        farmacia_info = {
+            'nome_farmacia': medicamento.farmacia.nome,
+            'preco': medicamento.preco,
+            'endereco': medicamento.farmacia.endereco,
+            'telefone': medicamento.farmacia.telefone,
+        }
+        resultados.append(farmacia_info)
+
+    return render(request, 'busca.html', {'resultados': resultados})
