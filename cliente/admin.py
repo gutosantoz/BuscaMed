964db5f7130ab378fea2fce8b2c52c2fce885657
veemon18 <lucasdrain18@hotmail.com>
2025-06-28 21:49:36 -0300

    Update admin.py

--- cliente/admin.py
+++ cliente/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Cliente
 
-# Register your models here.
+class ClienteAdmin(UserAdmin):
+    model = Cliente
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('telefone', 'endereco')}),
+    )
+
+admin.site.register(Cliente, ClienteAdmin)
