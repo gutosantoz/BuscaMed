900ffeb979db49f3ea1cacef0615f65a7308650b
veemon18 <lucasdrain18@hotmail.com>
2025-06-28 21:52:01 -0300

    Update admin.py

--- cliente/admin.py
+++ cliente/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Cliente
+from .forms import ClienteCreationForm
 
 class ClienteAdmin(UserAdmin):
+    add_form = ClienteCreationForm
     model = Cliente
+    list_display = ['username', 'email', 'telefone', 'endereco', 'is_staff']
     fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('telefone', 'endereco')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
         (None, {'fields': ('telefone', 'endereco')}),
     )
 
