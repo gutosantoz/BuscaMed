19572136395b630ec42554982a5cc1149fbfaaf5
veemon18 <lucasdrain18@hotmail.com>
2025-06-28 21:38:25 -0300

    Update models.py

--- cliente/models.py
+++ cliente/models.py
@@ -1,11 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
-class Cliente(models.Model):
-    nome = models.CharField(max_length=255)
-    cel  = models.CharField(max_length=255)
+class Cliente(AbstractUser):
+    telefone = models.CharField(max_length=20, blank=True)
     email = models.EmailField(unique=True)
-    endereco  = models.CharField(max_length=255)
+    nome = models.CharField(max_length=255)
+    endereco = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
-        return self.nome
+        return self.username
