811fb7d61d6823841fc8194f805bf84eda9268f9
veemon18 <lucasdrain18@hotmail.com>
2025-06-28 22:42:36 -0300

    Update tests.py

--- busca/tests.py
+++ busca/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Farmacia, Medicamento
 
-# Create your tests here.
+class MedicamentoTests(TestCase):
+    def test_create_medicamento(self):
+        farmacia = Farmacia.objects.create(nome="Farmácia Teste", endereco="Rua Teste, 123")
+        medicamento = Medicamento.objects.create(nome="Dipirona", nome_generico="Dipirona Sódica", preco=5.50, farmacia=farmacia)
+        self.assertEqual(medicamento.nome, 'Dipirona')
+        self.assertEqual(medicamento.farmacia.nome, 'Farmácia Teste')
